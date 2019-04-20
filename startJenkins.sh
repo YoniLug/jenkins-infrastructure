45125fc1def9b4e5ae967302bf25e2cfc796e933
Yoni Lugassi <yonilugassi@gmail.com>
2019-04-21 00:49:14 +0300

    adding parameters
    adding parameters to data volume location , jenkins host , container name

--- startJenkins.sh
+++ startJenkins.sh
@@ -1,12 +1,20 @@
+JENKINS_DATA_VOLUME=$1
+JENKINS_CONTAINER_NAME=$2
+JENKINS_SERVER_HOST=$3
+
+if ! [ -d ${JENKINS_DATA_VOLUME} ] ; then
+    mkdir ${JENKINS_DATA_VOLUME}
+fi
+
 docker run -d \
-    --name jenkins-home \
+    --name ${JENKINS_CONTAINER_NAME} \
     -u jenkins \
     --restart always \
     -p 8080:8080 \
     -p 50000:50000 \
-    -v /opt/jenkins:/var/jenkins_home \
+    -v ${JENKINS_DATA_VOLUME}:/var/jenkins_home \
     --env JAVA_OPTS="\
-        -Dhudson.footerURL=http://18.236.149.142 \
+        -Dhudson.footerURL=http://${JENKINS_SERVER_HOST} \
         -Duser.timezone=Asia/Jerusalem \
         -Dhudson.tasks.MailSender.SEND_TO_UNKNOWN_USERS=true \
         -Dhudson.tasks.MailSender.SEND_TO_USERS_WITHOUT_READ=true\
