a50f0522e4ec4074c7476783ceea86080dd5e76e
Yoni Lugassi <yonilugassi@gmail.com>
2019-04-22 06:54:57 +0300

    build jenkins image with docker client before publish

--- startJenkins.sh
+++ startJenkins.sh
@@ -6,6 +6,8 @@ if ! [ -d ${JENKINS_DATA_VOLUME} ] ; then
     mkdir ${JENKINS_DATA_VOLUME}
 fi
 
+docker build -t jenkins-docker .
+
 docker run -d \
     --name ${JENKINS_CONTAINER_NAME} \
     -u jenkins \
@@ -13,10 +15,11 @@ docker run -d \
     -p 8080:8080 \
     -p 50000:50000 \
     -v ${JENKINS_DATA_VOLUME}:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     --env JAVA_OPTS="\
         -Dhudson.footerURL=http://${JENKINS_SERVER_HOST} \
         -Duser.timezone=Asia/Jerusalem \
         -Dhudson.tasks.MailSender.SEND_TO_UNKNOWN_USERS=true \
         -Dhudson.tasks.MailSender.SEND_TO_USERS_WITHOUT_READ=true\
     " \
-    jenkins/jenkins:lts
+    jenkins-docker
